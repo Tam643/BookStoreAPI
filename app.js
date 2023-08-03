a4c87ba40e0bc8c934aa8275d2384052ae3e9732
Tum <tamvalove@gmail.com>
2023-08-03 13:09:17 +0700

    Connect to MongoDB Database

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
-
+const mongoose  = require('mongoose');
 const { indexRoutes, authorRoutes, genreRoutes, publisherRoutes, userRoutes, bookRoutes } = require('./routes');
 
 const app = express();
 
+// use middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// Database connection
+require('./configs/db.config')(mongoose);
+
+// Define route
 app.use('/', indexRoutes);
 app.use('/author', authorRoutes);
 app.use('/genre', genreRoutes);
@@ -22,20 +32,13 @@ app.use('/users', userRoutes);
 app.use('/book', bookRoutes);
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
 // error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
-
-    const error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500).json(error);
-}
-);
+app.use(function(err, req, res, next){
+    res.status(err.status || 500).json({ error: err.message });
+  });
+// catch 404
+app.use(function(req, res){
+    res.status(404).json({ error: "Not Found" })
+  });
 
 module.exports = app;
--- configs/db.config.js
+++ configs/db.config.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
-
-const { DB_URI } = process.env;
-
-exports.connect = () => {
-    // Connecting to Databasse
-    mongoose.connect(DB_URI)
-    .then(()=>{
-        console.log("Database is connected");
-    })
-    .catch((error) => {
-        console.log("Error connecting to database")
-        console.error(error);
-        process.exit(1);
-    });
-}
+require("dotenv").config();
+module.exports = (mongoose)=>{
+  mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+};
